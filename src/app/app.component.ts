ffd3a17a1ed72cda1e2a2e538c344e7cb974cded
Benoit-Hayet <hayetbenoit@gmail.com>
2024-03-04 08:37:33 +0100

    Ok

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CatalogComponent } from './catalog/catalog.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,CatalogComponent,],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'WorkShopDirectives';
+}
---
+++ src/app/catalog/catalog.component.ts
@@ -0,0 +1,22 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { Product } from './product.models';
+
+@Component({
+  selector: 'app-catalog',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './catalog.component.html',
+  styleUrl: './catalog.component.scss',
+})
+export class CatalogComponent {
+  listProducts: Product[] = [
+    { id: 1, name: 'Banane', prix: 2, promo: 11 },
+    { id: 2, name: 'Pomme', prix: 3, promo: 15 },
+    { id: 2, name: 'Poire', prix: 4, promo:0 },
+  ];
+
+  trackByProductId(index: number, product: Product): number {
+    return product.id;
+}
+}
